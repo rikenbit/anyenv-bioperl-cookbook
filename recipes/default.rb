db28f336afe662e1031f10eabaa84221e1259265
Manabu ISHII <manabu.ishii.rb@gmail.com>
2014-06-06 17:38:19 +0900

    install as user

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,13 @@
 package "libdb5.1-dev"
-execute "install cpanm" do
+execute "install plenv install-cpanm" do
+  user "vagrant"
+  group "vagrant"
   command "export PATH=\"$HOME/.anyenv/bin:$PATH\";eval \"$(anyenv init -)\";plenv install-cpanm"
   action :run
 end
-execute "install BioPerl 1.6.922" do
+execute "install BioPerl 1.6.922 not 1.6.923" do
+  user "vagrant"
+  group "vagrant"
   command "export PATH=\"$HOME/.anyenv/bin:$PATH\";eval \"$(anyenv init -)\";plenv exec cpanm CJFIELDS/BioPerl-1.6.922.tar.gz CJFIELDS/BioPerl-1.6.922.tar.gz"
   action :run
 end
